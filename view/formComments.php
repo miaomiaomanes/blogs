ceb6f2f1466fd5ec7dd179e9ed7a27b3695d816e
Miaomiao <miaomiao@valentins-macbook-pro.home>
2023-02-28 14:11:48 +0100

    Minor

--- view/formComments.php
+++ view/formComments.php
@@ -1,7 +1,7 @@
 <form action="?p=addCommentForm" method="post">
 
 Reviews : 
-<br><textarea name="review" id="review" cols="60" rows="2"></textarea><br>
+<br><textarea class='border-solid border-2 'name="review" id="review" cols="60" rows="2"></textarea><br>
     <input type="hidden" value="<?= $article['id_article'] ?>" name="id_article" id="id_article">
     
         <button class=" btn btn-primary">Submit</button>
--- view/formContact.php
+++ view/formContact.php
@@ -1,47 +1,55 @@
-<div class="isolate bg-white py-24 px-6 sm:py-32 lg:px-8">
-    <div class="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
-        <svg class="relative left-1/2 -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-40rem)] sm:h-[42.375rem]" viewBox="0 0 1155 678">
-            <path fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)" fill-opacity=".3" d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z" />
-            <defs>
-                <linearGradient id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533" x1="1155.49" x2="-78.208" y1=".177" y2="474.645" gradientUnits="userSpaceOnUse">
-                    <stop stop-color="#9089FC" />
-                    <stop offset="1" stop-color="#FF80B5" />
-                </linearGradient>
-            </defs>
-        </svg>
+
+
+<div class="hero min-h-screen bg-base-200">
+    <div class="hero-content text-center">
+        <div class="max-w-md">
+            <h1 class="text-5xl font-bold">Contacts</h1>
+            <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <form action="" method="post">
+
+                <div class="card-body">
+                    <form action="" method="post">
+                        <div class="form-control">
+                            <label class="label">
+                                <span class="label-text">Firs Name</span>
+                            </label>
+                            <input type="text" name="prenom" id="prenom" placeholder="Title" class="input input-bordered" />
+                        </div>
+                        <div class="form-control">
+                            <label class="label">
+                                <span class="label-text">Last</span>
+                            </label>
+                            <input type="text" name="nom" id="nom" placeholder="Title" class="input input-bordered" />
+
+                        </div>
+
+                        <div class="form-control">
+                            <label class="label">
+                                <span class="label-text">Email</span>
+                            </label>
+                            <input type="text" name="email" id="email" placeholder="Title" class="input input-bordered" />
+                        </div>
+
+                        <div class="form-control">
+                            <label class="label">
+                                <span class="label-text">Message</span>
+                            </label>
+                            <textarea class='border-solid border-2 ' name="message_text" id="message_text" cols="60" rows="5"></textarea>
+                        </div>
+
+                
+
+
+                        <div class="form-control mt-6">
+                            <button class="btn btn-primary">Submit</button>
+                        </div>
+
+
+
+                </div>
+            </form>
+
+
+        </div>
     </div>
-    <div class="mx-auto max-w-2xl text-center">
-        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact sales</h2>
-        <p class="mt-2 text-lg leading-8 text-gray-600">Aute magna irure deserunt veniam aliqua magna enim voluptate.</p>
-    </div>
-    <form action="" method="POST" class="mx-auto mt-16 max-w-xl sm:mt-20">
-        <div class="grid grid-cols-1 gap-y-6 gap-x-8 sm:grid-cols-2">
-            <div>
-                <label for="first-name" class="block text-sm font-semibold leading-6 text-gray-900">First name</label>
-                <div class="mt-2.5">
-                    <input type="text" name="prenom" id="prenom" autocomplete="given-name" class="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                </div>
-            </div>
-            <div>
-                <label for="last-name" class="block text-sm font-semibold leading-6 text-gray-900">Last name</label>
-                <div class="mt-2.5">
-                    <input type="text" name="nom" id="nom" autocomplete="family-name" class="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                </div>
-            </div>
-            <div class="sm:col-span-2">
-                <label for="Email" class="block text-sm font-semibold leading-6 text-gray-900">Email</label>
-                <div class="mt-2.5">
-                    <input type="text" name="email" id="email" autocomplete="email" class="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                </div>
-            </div>
-            <div class="sm:col-span-2">
-                <label for="message" class="block text-sm font-semibold leading-6 text-gray-900">Message</label>
-                <div class="mt-2.5">
-                    <textarea name="message_text" id="message_text" rows="4" class="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
-                </div>
-            </div>
-            <div class="mt-10 pb-20">
-                <button type="submit" class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Let's talk</button>
-            </div>
-    </form>
 </div>
