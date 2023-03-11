a1939ad94f11ce3a4ff64d3f2148a25303972e46
Miaomiao <miaomiao@valentins-macbook-pro.home>
2023-03-11 12:10:37 +0100

    debug the footer

--- view/articleViewFullPage.php
+++ view/articleViewFullPage.php
@@ -5,16 +5,12 @@
             <p class="mt-2 text-lg leading-8 text-gray-600">Explore the world news with your kids</p>
         </div>
         <div class="mx-auto  grid max-w-2xl grid-cols-1 gap-y-16 gap-x-1 border-t border-gray-200  sm:mt-10 sm:pt-5 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-        
 
-            <?php include_once('view/articleView.php');?>
-            <?php include_once('view/dernierArticlesViews.php'); ?>
- 
+
+           <?php include_once('view/articleView.php'); ?>
+          <?php include_once('view/dernierArticlesViews.php'); ?>
+
             <!-- More posts... -->
         </div>
-
-        
     </div>
 </div>
-
-
--- view/formComments.php
+++ view/formComments.php
@@ -1,7 +1,7 @@
 <form action="?p=addCommentForm" method="post">
 
-R
-<article class="border-solid ml-20 mb-20 border-2 flex max-w-xl flex-col items-start justify-between mt-20">
+
+    <article class="border-solid ml-20 mb-20 border-2 flex max-w-xl flex-col items-start justify-between mt-20">
         <div class="relative rounded-lg mt-4 flex items-center gap-x-4">
             <img src="assets/img/logo.png" alt="" class="h-10 w-10 rounded-full bg-gray-50">
             <div class="text-sm leading-4">
@@ -12,18 +12,19 @@ R
                         <span><?= $_SESSION['nom'] ?></span>
                     </a>
                 </p>
-                
+
             </div>
-            
+
         </div>
         <div class="divider max-w-l "></div>
-        <div class="text-gray-600"> 
-        Reviews : 
-<br><textarea class='border-solid border-2 'name="review" id="review" cols="60" rows="2"></textarea><br>
-    <input type="hidden" value="<?= $article['id_article'] ?>" name="id_article" id="id_article">
-    
-        <button class=" btn btn-primary">Submit</button>
-    
-</form></div>
+        <div class="text-gray-600">
+            Reviews :
+            <br><textarea class='border-solid border-2 ' name="review" id="review" cols="60" rows="2"></textarea><br>
+            <input type="hidden" value="<?= $article['id_article'] ?>" name="id_article" id="id_article">
+
+            <button class=" btn btn-primary">Submit</button>
+        </div>
 
     </article>
+
+</form>
